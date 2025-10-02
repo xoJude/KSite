82a615ecbf76993359311e40a1fba6ca72a08775
Jude Gigon <jude.gigon@icloud.com>
2025-10-03 00:59:57 +0200

    csss

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
-<?php include 'header.php'; ?>
-    <h2>Bienvenue dans notre cabinet d'avocats ! </h2>
-    <p>Avec le cabinet Kessentini, faites le choix d'une expertise fiable et engagée pour relever vos défis juridiques.</p>
-    <p>Notre approche : stratégique, personnalisée, et résolument tournée vers vos intérêts.</p>
 
-<?php include 'footer.php'; ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cabinet Kessentini - Avocats</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include 'header.php'; ?>
+
+    <main>
+        <h2>Bienvenue testttt dans notre cabinet d'avocats !</h2>
+        <p>Avec le cabinet Kessentini, faites le choix d'une expertise fiable et engagée pour relever vos défis juridiques.</p>
+        <p>Notre approche : stratégique, personnalisée, et résolument tournée vers vos intérêts.</p>
+    </main>
+
+    <?php include 'footer.php'; ?>
+</body>
+</html>
